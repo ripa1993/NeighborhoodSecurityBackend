48dde72b67b1e2c8e52fbd60fe2dada395303988
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-04-10 18:40:02 +0200

    Authentication

--- src/main/java/it/polimi/moscowmule/neighborhoodsecurity/authentication/Authenticator.java
+++ src/main/java/it/polimi/moscowmule/neighborhoodsecurity/authentication/Authenticator.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
+import it.polimi.moscowmule.neighborhoodsecurity.utilities.ProjectConstants;
 import it.polimi.moscowmule.neighborhoodsecurity.utilities.database.Database;
 import it.polimi.moscowmule.neighborhoodsecurity.utilities.exceptions.AuthorizationDBException;
 import it.polimi.moscowmule.neighborhoodsecurity.utilities.exceptions.NoUserFoundException;
@@ -15,18 +16,20 @@ import it.polimi.moscowmule.neighborhoodsecurity.utilities.exceptions.SecretDBEx
 public class Authenticator {
 	/**
 	 * Retrieves user id given the token
+	 * 
 	 * @param authToken
 	 * @return the user is
 	 * @throws AuthorizationDBException
-	 * @throws NoUserFoundException if the authToken is of no user
+	 * @throws NoUserFoundException
+	 *             if the authToken is of no user
 	 */
 	public static int getUserId(String authToken) throws AuthorizationDBException, NoUserFoundException {
 		Connection connection;
 		try {
 			connection = Database.getConnection();
 			PreparedStatement getStmt = connection.prepareStatement(
-					"SELECT ID FROM gsx95369n3oh2zo6.authorization WHERE TOKEN = ? AND ISVALID = 1", ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY);
+					"SELECT ID FROM gsx95369n3oh2zo6.authorization WHERE TOKEN = ? AND ISVALID = 1",
+					ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			getStmt.clearParameters();
 			getStmt.setString(1, authToken);
 			ResultSet result = getStmt.executeQuery();
@@ -44,9 +47,12 @@ public class Authenticator {
 
 	/**
 	 * Check if an id is superuser
-	 * @param id of the user
+	 * 
+	 * @param id
+	 *            of the user
 	 * @return the status
-	 * @throws NoUserFoundException if the id is of no user
+	 * @throws NoUserFoundException
+	 *             if the id is of no user
 	 * @throws AuthorizationDBException
 	 */
 	public static boolean isSuperuser(int id) throws NoUserFoundException, AuthorizationDBException {
@@ -75,10 +81,12 @@ public class Authenticator {
 
 	/**
 	 * Checks if a username-password pair match a valid user id
+	 * 
 	 * @param username
 	 * @param password
 	 * @return the id of the user, if there's a match
-	 * @throws NoUserFoundException if no match
+	 * @throws NoUserFoundException
+	 *             if no match
 	 * @throws SecretDBException
 	 */
 	public static int checkPassword(String username, String password) throws NoUserFoundException, SecretDBException {
@@ -92,7 +100,7 @@ public class Authenticator {
 			getStmt.setString(1, username);
 			getStmt.setString(2, password);
 			ResultSet result = getStmt.executeQuery();
-			if (result.next()){
+			if (result.next()) {
 				return result.getInt(1);
 			} else {
 				// wrong pair
@@ -102,49 +110,70 @@ public class Authenticator {
 			throw new SecretDBException("ERROR when checking username and password", e);
 		}
 	}
-	
+
 	/**
 	 * Generates a new token for the given user id
-	 * @param id the user id
+	 * 
+	 * @param id
+	 *            the user id
 	 * @return the generated token
 	 * @throws AuthorizationDBException
 	 */
-	public static String generateToken(int id) throws AuthorizationDBException{
+	public static String generateToken(int id) throws AuthorizationDBException {
 		Connection connection;
 		try {
 			connection = Database.getConnection();
-			PreparedStatement updStmt = connection.prepareStatement("UPDATE gsx95369n3oh2zo6.authorization SET TOKEN = ?, ISVALID = 1 WHERE ID = ?");
+			PreparedStatement updStmt = connection
+					.prepareStatement("UPDATE gsx95369n3oh2zo6.authorization SET TOKEN = ?, ISVALID = 1 WHERE ID = ?");
 			String token = UUID.randomUUID().toString();
 			updStmt.setString(1, token);
 			updStmt.setInt(2, id);
 			updStmt.executeUpdate();
-			
+
 			return token;
-			
+
 		} catch (ClassNotFoundException | URISyntaxException | SQLException e) {
 			throw new AuthorizationDBException("ERROR when creating token", e);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Invalidates the token of the user
-	 * @param id of the user
+	 * 
+	 * @param id
+	 *            of the user
 	 * @return true if all is ok
 	 * @throws AuthorizationDBException
 	 */
-	public static boolean invalidateToken(int id) throws AuthorizationDBException{
+	public static boolean invalidateToken(int id) throws AuthorizationDBException {
 		Connection connection;
 		try {
 			connection = Database.getConnection();
-			PreparedStatement updStmt = connection.prepareStatement("UPDATE gsx95369n3oh2zo6.authorization SET ISVALID = 0 WHERE ID = ?");
+			PreparedStatement updStmt = connection
+					.prepareStatement("UPDATE gsx95369n3oh2zo6.authorization SET ISVALID = 0 WHERE ID = ?");
 			updStmt.setInt(1, id);
 			updStmt.executeUpdate();
-			
+
 			return true;
-			
+
 		} catch (ClassNotFoundException | URISyntaxException | SQLException e) {
 			throw new AuthorizationDBException("ERROR when invalidating token", e);
+		}
+	}
+
+	public static boolean isServiceKeyValid(String serviceKey) {
+		if (ProjectConstants.SERVICE_KEYS.contains(serviceKey))
+			return true;
+		return false;
+	}
+	
+	public static boolean isAuthTokenValid(String authToken){
+		try {
+			getUserId(authToken);
+			return true;
+		} catch (AuthorizationDBException | NoUserFoundException e) {
+			return false;
 		}
 	}
 }
--- src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/ProjectConstants.java
+++ src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/ProjectConstants.java
@@ -1,7 +1,14 @@
 package it.polimi.moscowmule.neighborhoodsecurity.utilities;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ProjectConstants {
 	public static final String EVENTS_BASE_URL = "https://thawing-taiga-87659.herokuapp.com/events";
 	public static final String USERS_BASE_URL = "https://thawing-taiga-87659.herokuapp.com/users";
 	
+	public static final String SERVICE_KEY = "service_key";
+	public static final String AUTH_TOKEN = "auth_token";
+	
+	public static final List<String> SERVICE_KEYS = Arrays.asList("moscowmule");
 }

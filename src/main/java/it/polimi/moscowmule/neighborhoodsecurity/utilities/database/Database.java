b19567a9917b5ae0733a37935e76692364e711fc
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-04-11 16:30:42 +0200

    disabling ssl

--- src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/database/Database.java
+++ src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/database/Database.java
@@ -24,7 +24,7 @@ public class Database {
 	    String username = jdbUri.getUserInfo().split(":")[0];
 	    String password = jdbUri.getUserInfo().split(":")[1];
 	    String port = String.valueOf(jdbUri.getPort());
-	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();	    
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath()+"?useSSL=false";	    
 	    
 	    return DriverManager.getConnection(jdbUrl, username, password);
 	}

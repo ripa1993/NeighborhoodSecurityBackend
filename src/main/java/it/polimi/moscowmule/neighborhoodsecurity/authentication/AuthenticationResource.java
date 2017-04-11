42b7c8f1668349e4d4738b78a712f0bae1d70ea1
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-04-11 16:41:36 +0200

    Javadoc for auth resource

--- src/main/java/it/polimi/moscowmule/neighborhoodsecurity/authentication/AuthenticationResource.java
+++ src/main/java/it/polimi/moscowmule/neighborhoodsecurity/authentication/AuthenticationResource.java
@@ -20,11 +20,20 @@ import it.polimi.moscowmule.neighborhoodsecurity.utilities.exceptions.SecretDBEx
 @Path("auth")
 public class AuthenticationResource {
 
+	/**
+	 * Login using username and password
+	 * 
+	 * @param username
+	 * @param password
+	 * @return OK, with the authorization token; UNAUTHORIZED, if the login is
+	 *         incorrect; INTERNAL_SERVER_ERROR, if there was a problem with the
+	 *         request
+	 */
 	@POST
 	@Path("/classic")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response loginClassic(@FormParam("username") String username, @FormParam("password") String password ) {
+	public Response loginClassic(@FormParam("username") String username, @FormParam("password") String password) {
 		try {
 			int userId = Authenticator.checkPassword(username, password);
 			String authToken = Authenticator.generateToken(userId);
@@ -34,30 +43,44 @@ public class AuthenticationResource {
 			toBeReturned.setUsername(username);
 			return Response.ok(toBeReturned).build();
 		} catch (NoUserFoundException e) {
-			return Response.status(Status.UNAUTHORIZED).entity(new Message("AUTHORIZATION", "Login is incorrect")).build();
+			return Response.status(Status.UNAUTHORIZED).entity(new Message("AUTHORIZATION", "Login is incorrect"))
+					.build();
 		} catch (SecretDBException e) {
-			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage())).build();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage()))
+					.build();
 		} catch (AuthorizationDBException e) {
-			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage())).build();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage()))
+					.build();
 		} catch (NoTokenCreatedException e) {
-			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", "Problem in generating the token")).build();
+			return Response.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(new Message("DATABASE", "Problem in generating the token")).build();
 		}
-		
+
 	}
-	
+
+	/**
+	 * Logout by invalidating a token
+	 * 
+	 * @param authToken
+	 * @return OK, if the provided token was valid and now it is no more;
+	 *         UNAUTHORIZED, if the token is already invalid;
+	 *         INTERNAL_SERVER_ERROR, if there was a problem with your request
+	 */
 	@POST
 	@Path("/logout")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response logout(@HeaderParam(ProjectConstants.AUTH_TOKEN) String authToken){
+	public Response logout(@HeaderParam(ProjectConstants.AUTH_TOKEN) String authToken) {
 		try {
 			int userId = Authenticator.getUserId(authToken);
 			Authenticator.invalidateToken(userId);
 			return Response.ok(new Message("AUTHORIZATION", "Logged out, discard your token")).build();
 		} catch (AuthorizationDBException e) {
-			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage())).build();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new Message("DATABASE", e.getMessage()))
+					.build();
 		} catch (NoUserFoundException e) {
-			return Response.status(Status.UNAUTHORIZED).entity(new Message("AUTHORIZATION", "Your auth token is already invalid")).build();
+			return Response.status(Status.UNAUTHORIZED)
+					.entity(new Message("AUTHORIZATION", "Your auth token is already invalid")).build();
 		}
 	}
-	
+
 }

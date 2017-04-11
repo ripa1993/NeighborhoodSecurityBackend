31f4e9d3067925a1bd36da26d8d54722fe755a9a
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-04-11 16:19:18 +0200

    removing warning

--- src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/exceptions/NoTokenCreatedException.java
+++ src/main/java/it/polimi/moscowmule/neighborhoodsecurity/utilities/exceptions/NoTokenCreatedException.java
@@ -2,4 +2,9 @@ package it.polimi.moscowmule.neighborhoodsecurity.utilities.exceptions;
 
 public class NoTokenCreatedException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6419768192425811212L;
+
 }
